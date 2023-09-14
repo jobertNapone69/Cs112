4e5c49d203601f780040dd54b0f12509d4c80df6
jobertNapone69 <145018357+jobertNapone69@users.noreply.github.com>
2023-09-14 05:14:25 -0700

    Add files via upload
    Lumanas and Napone Activiy 1

---
+++ Activity 1 Napone and Lumanas.py
@@ -0,0 +1,17 @@
+# This is my account details
+print("Jobert Napone")
+
+print("B.S In Computer Science")
+
+print("CS1D")
+
+print("18")
+
+print("August 30, 2005")
+
+print("Purok-9 Damilag, Manolo Fortich, Bukidnon")
+
+
+
+
+
